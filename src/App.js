9ca0a6360694856f14f07d709bb54ce6a32457b2
Andrew Cetnarskyj <andrew.cetnarskyj@woodmac.com>
2020-12-31 16:14:20 +0000

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="container bg-gray-500">
+            <header className="centre w-6/12">
+                <h1>This is a heading</h1>
+                <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <a className="text-white r" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+                    Learn React
+                </a>
+            </header>
+        </div>
+    );
 }
 
 export default App;
